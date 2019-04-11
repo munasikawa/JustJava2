fb5ffeb22bacdf74ddab7fcfd27641b896a400be
munasikawa <munasikawa@gmail.com>
2019-04-11 13:01:41 +0300

    String concatenation

--- app/src/main/java/com/example/android/justjava/MainActivity.java
+++ app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -25,7 +25,18 @@ public class MainActivity extends AppCompatActivity {
      * This method is called when the order button is clicked.
      */
     public void submitOrder(View view) {
-        displayPrice(quantity * priceOfOneCoffeeCup);
+        int price = quantity * priceOfOneCoffeeCup;
+        String priceMessage = "Total: $" + price;
+        priceMessage = priceMessage + "\nThank You!";
+        displayMessage(priceMessage);
+    }
+
+    /**
+     * This method displays the given text on the screen.
+     */
+    private void displayMessage(String message) {
+        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
+        priceTextView.setText(message);
     }
 
     /**
@@ -43,6 +54,7 @@ public class MainActivity extends AppCompatActivity {
         quantity = quantity - 1;
         display(quantity);
     }
+
 
     /**
      * This method displays the given quantity value on the screen.
